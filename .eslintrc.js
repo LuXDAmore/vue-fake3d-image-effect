8f1a82dc7996d7bcc55a43997779b537026a26af
Luca Iaconelli <lucabelli@msn.com>
2019-10-15 09:21:32 +0200

    CHORE: Rules for eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -46,6 +46,10 @@ module.exports = {
         ],
         'spaced-comment': [ 'warn', 'always' ],
         'prefer-const': 'warn',
+        'no-useless-rename': [
+            'warn',
+            {},
+        ],
         'rest-spread-spacing': [
             'warn',
             'always',
